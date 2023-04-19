aefb7531bd456c64dd50439c5839861b9204f815
MeshallAl <malshalaan@ucsd.edu>
2023-04-19 16:20:42 -0700

    fixed reverseinplace

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -6,6 +6,7 @@ public class ArrayExamples {
   static void reverseInPlace(int[] arr) {
     for(int i = 0; i < arr.length; i += 1) {
       arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = temp;
     }
   }
 
--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,20 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
+  @Test
+  public void testReversedInPlace2(){
+    int[] input1 = {1,2,3};
+    int[] input2 = {3,2,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals( input2, input1);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+
 }
